90687e4fd4a6f19fc8828a20d7ba9b2413594bb2
tina <tina.safiniaynaini@svt.se>
2022-02-15 10:29:17 +0100

    fixed pagination

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -5,6 +5,7 @@ import { Block, Text, Col, Row, Icon } from "vcc-ui";
 function Photo(props: { data: any[] }) {
   const itemsPerPage = 4;
   const [page, setPage] = useState(1);
+  const [emptyPage, setEmptyPage] = useState(false);
   const indexOfLastItem = page * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = props.data.slice(indexOfFirstItem, indexOfLastItem);
@@ -33,19 +34,37 @@ function Photo(props: { data: any[] }) {
   for (let i = 1; i <= Math.ceil(props.data.length / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
+  let lastPage = Number(pageNumbers.slice(-1));
+  let firstPage = Number(pageNumbers[0]);
 
   return (
     <div>
       <Row align="center">{renderItems} </Row>
+      {page > lastPage || page < firstPage
+        ? setEmptyPage(true)
+        : ('')}
 
       <Row align="end" aria-label="Page navigation">
-        <a href="#" aria-label="Previous" onClick={() => setPage(page - 1)}>
-          <Icon type="media-previous-32" />
-        </a>
-
-        <a href="#" aria-label="Next" onClick={() => setPage(page + 1)}>
-          <Icon type="media-next-32" />
-        </a>
+        {!emptyPage ? (
+          <a
+            href="#"
+            aria-label="Previous"
+            onClick={() => {
+              page === firstPage ? setPage(lastPage) : setPage(page - 1);
+            }}
+          >
+            <Icon type="media-previous-32" />
+          </a>
+        ) : (
+          (setPage(lastPage), setEmptyPage(false))
+        )}
+        {!emptyPage ? (
+          <a href="#" aria-label="Next" onClick={() => setPage(page + 1)}>
+            <Icon type="media-next-32" />
+          </a>
+        ) : (
+          (setPage(firstPage), setEmptyPage(false))
+        )}
       </Row>
     </div>
   );
